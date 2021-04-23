74f7c7c72de79a65ae6f64efd8ede451f7efd8ed
Guido Neele <guido@crisisconnect.nl>
2021-04-23 14:57:37 +0200

    Sample project XCT TouchEffect Bug

---
+++ Sample/Sample/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Forms;
+
+namespace Sample
+{
+  public class MainViewModel : ObservableObject
+  {
+    public MainViewModel()
+    {
+      this.MyCommand = new Command(Go);
+    }
+
+    public ICommand MyCommand { get; set; }
+
+    private void Go(object obj)
+    {
+      Application.Current.MainPage.DisplayAlert("Alert", "Button was clicked!", "Cancel");
+    }
+  }
+}
